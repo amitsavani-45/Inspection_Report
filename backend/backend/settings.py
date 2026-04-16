9a93956f8c1c3685de659a2bf2b68a5acb2d2038
amitsavani-45 <apsavani4518@gmail.com>
2026-04-16 22:35:00 +0530

    Backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,7 +4,7 @@ Django settings for backend project.
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
